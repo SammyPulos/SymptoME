5b581da58a334bd84272f886742deb01ee32fc5b
scuPulos <44333074+scuPulos@users.noreply.github.com>
2020-04-30 13:43:54 -0700

    Confirming Initial Test

--- SymptoME/src/symptome/SymptoME.java
+++ SymptoME/src/symptome/SymptoME.java
@@ -5,6 +5,7 @@ public class SymptoME {
     public static void main(String[] args) {
         System.out.println("Welcome to SymptoME");
         System.out.println("Testing");
+        System.out.println("Confirming test");
     }
     
 }
