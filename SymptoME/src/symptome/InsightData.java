2c64f9d5e35115133167a94fadaef409b3e57c0b
scuPulos <44333074+scuPulos@users.noreply.github.com>
2020-05-15 20:19:14 -0700

    Added insights labels and InsightData

---
+++ SymptoME/src/symptome/InsightData.java
@@ -0,0 +1,31 @@
+package symptome;
+
+public class InsightData {
+    
+    public InsightData() {
+        // TODO
+    }
+    
+    // TODO: actually figure out the percents
+    public double getPercentSameSymptoms() {
+        return 0;
+    }
+    public double getPercentSameSymptomsTested() {
+        return 1;
+    }
+    public double getPercentSameSymptomsPositive() {
+        return 2;
+    }
+    public double getPercentSameSymptomsNegative() {
+        return 3;
+    }
+    public double getPercentSameLocationTested() {
+        return 4;
+    }
+    public double getPercentSameLocationPositive() {
+        return 5;
+    }
+    public double getPercentSameLocationNegative() {
+        return 6;
+    }
+}
--- SymptoME/src/symptome/InsightsScreen.java
+++ SymptoME/src/symptome/InsightsScreen.java
@@ -1,5 +1,6 @@
 package symptome;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
@@ -37,10 +38,36 @@ public class InsightsScreen implements Screen{
         });
         
         screenPanel.add(titleLabel);
+        
+        SetupInsights();
+        
         screenPanel.add(homeButton);
         screenPanel.add(logoutButton);
         
         return screenPanel;
+    }
+    
+    private Screen SetupInsights() {
+        if (screenPanel == null) { return this; }
+        
+        InsightData insightData = new InsightData(); // TODO: need to actually get/generate this
+        
+        JLabel percentSame = new JLabel("<html><br/>Overall " + insightData.getPercentSameSymptoms() + "% users have recorded the same symptoms as yours at some point</html>");
+        JLabel percentSameSymptomsTested = new JLabel("<html><br/>Out of people with symptoms matching your most recent report " + insightData.getPercentSameSymptomsTested() + "% have been tested for COVID-19</html>");
+        JLabel percentSameSymptomsResults = new JLabel("    Out of these " + insightData.getPercentSameSymptomsPositive() + "% have tested positive and " + insightData.getPercentSameSymptomsNegative() + "% have tested negative");
+        JLabel percentSameLocationTested = new JLabel("<html><br/>In your area " + insightData.getPercentSameLocationTested() + "% of users have been tested for COVID-19</html>");
+        JLabel percentSameLocationResults = new JLabel("    Out of these " + insightData.getPercentSameLocationPositive() + "% have tested positive and " + insightData.getPercentSameLocationNegative() + "% have tested negative");
+        JLabel feelingLabel = new JLabel("<html><br/>Feeling trend: [REMEMBER TO ADD A GRAPH]</html>");
+        feelingLabel.setForeground(Color.red);
+        
+        screenPanel.add(percentSame);
+        screenPanel.add(percentSameSymptomsTested);
+        screenPanel.add(percentSameSymptomsResults);
+        screenPanel.add(percentSameLocationTested);
+        screenPanel.add(percentSameLocationResults);
+        screenPanel.add(feelingLabel);
+        
+        return this;
     }
     
     private Screen handleHomeButtonPressed() {
